f971af38d0c45736636db3d689a675a259ed23ab
Alexis Kofman <alexis.kofman@gmail.com>
2015-10-29 16:47:51 +0100

    initial commit

---
+++ src/disable-bitcode.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+/*
+ * Disable bitcode for ios9 projects.
+ */
+
+var xcode = require('xcode');
+var fs = require('fs');
+var util = require('cordova-lib/src/cordova/util');
+var ConfigParser = require('cordova-lib/src/configparser/ConfigParser');
+
+var projectName = new ConfigParser(util.projectConfig(util.isCordova())).name();
+var projectPath = './platforms/ios/' + projectName + '.xcodeproj/project.pbxproj';
+var myProj = xcode.project(projectPath);
+
+myProj.parse(function (err) {
+  if(err){
+    console.log('Error: ' + JSON.stringify(err));
+  }
+  else{
+    myProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
+    fs.writeFileSync(projectPath, myProj.writeSync());
+    console.log('✔ BITCODE disable');
+  }
+});
