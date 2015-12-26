1d8f7db001963ad1e986d12325286f65af558dd1
Alexis Kofman <alexis.kofman@gmail.com>
2015-12-26 16:21:32 +0100

    well execute this script for all the configured hooks

--- src/disable-bitcode.js
+++ src/disable-bitcode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 /*
- * Disable bitcode for ios9 projects.
- */
+* Disable bitcode for ios9 projects.
+*/
 
 var xcode = require('xcode');
 var fs = require('fs');
@@ -11,15 +11,18 @@ var ConfigParser = require('cordova-common/src/ConfigParser/ConfigParser');
 
 var projectName = new ConfigParser(util.projectConfig(util.isCordova())).name();
 var projectPath = './platforms/ios/' + projectName + '.xcodeproj/project.pbxproj';
-var myProj = xcode.project(projectPath);
 
-myProj.parse(function (err) {
-  if(err){
-    console.log('Error: ' + JSON.stringify(err));
-  }
-  else{
-    myProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
-    fs.writeFileSync(projectPath, myProj.writeSync());
-    console.log('✔ BITCODE disable');
-  }
-});
+module.exports = function() {
+  var myProj = xcode.project(projectPath);
+  
+  myProj.parse(function (err) {
+    if(err){
+      console.log('Error: ' + JSON.stringify(err));
+    }
+    else{
+      myProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
+      fs.writeFileSync(projectPath, myProj.writeSync());
+      console.log('✔ BITCODE disable');
+    }
+  });
+};
