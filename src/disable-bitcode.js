30774d768c8634c8e4cc42df68caac584b8691a2
Alexis Kofman <alexis.kofman@gmail.com>
2016-02-22 01:31:39 +0100

    rm semver dependency

--- src/disable-bitcode.js
+++ src/disable-bitcode.js
@@ -6,26 +6,31 @@
 
 var fs = require('fs');
 var xcode = require('xcode');
-var cmp = require('semver-compare');
+var path = require('path');
 
 module.exports = function(context) {
-  var projectName, projectPath, myProj, ConfigParser;
+  var projectName, projectPath, xcodeProj;
   var projectRoot = context.opts.projectRoot;
 
-  if(cmp(context.opts.cordova.version, '5.4.0') >= 0) {
-    ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
-  } else {
-    ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
-  }
-
-  projectName = new ConfigParser(projectRoot + '/config.xml').name();
-  projectPath = projectRoot + '/platforms/ios/' + projectName + '.xcodeproj/project.pbxproj';
-  myProj = xcode.project(projectPath);
+  projectName = getConfigParser(context, path.join(projectRoot, 'config.xml')).name();
+  pbxPath = path.join(projectRoot, 'platforms', 'ios', projectName + '.xcodeproj/project.pbxproj');
+  xcodeProj = xcode.project(pbxPath);
 
   //We need to use parseSync because async causes problems when other plugins
   //are handling pbxproj file.
-  myProj.parseSync();
-  myProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
-  fs.writeFileSync(projectPath, myProj.writeSync());
-  console.log('✔ BITCODE disable');
+  xcodeProj.parseSync();
+  xcodeProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
+  fs.writeFileSync(pbxPath, xcodeProj.writeSync());
 };
+
+function getConfigParser(context, config){
+  var semver = context.requireCordovaModule('semver');
+
+  if(semver.lt(context.opts.cordova.version, '5.4.0')) {
+    ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+  } else {
+    ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+  }
+
+  return new ConfigParser(config);
+}
